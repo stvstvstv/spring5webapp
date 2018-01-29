7db37ff0d51982d5a0967605e14db97847f287a3
stefan dumitru <dumitrustefan92@gmail.com>
2018-01-29 18:53:08 +0200

    Initial check

---
+++ spring5webapp/src/main/java/com/springframework/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,16 @@
+package com.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+	
+	public void getCacat() {
+		System.out.println("test");
+	}
+}
